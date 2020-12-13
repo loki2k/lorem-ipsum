273c94ca140d2734d9d853f41603fa22643e885f
loki2k <jeremylagarto@gmail.com>
2020-12-13 10:34:02 -0800

    Updates

---
+++ precache-manifest.a5fe3e65615e20f98d378c481ccd1e6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e390734eb53500dc104d1a04dfe3fc4",
+    "url": "/lorem-ipsum/index.html"
+  },
+  {
+    "revision": "62e688a87f2e287eb524",
+    "url": "/lorem-ipsum/static/css/main.9d994fcb.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/lorem-ipsum/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lorem-ipsum/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62e688a87f2e287eb524",
+    "url": "/lorem-ipsum/static/js/main.6aac333b.chunk.js"
+  },
+  {
+    "revision": "c4ba6ab66839d64706a0",
+    "url": "/lorem-ipsum/static/js/runtime-main.1cd389ed.js"
+  }
+]);
